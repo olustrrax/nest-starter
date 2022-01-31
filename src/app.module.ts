1763cf26cc8617ce7d7d63e0b3ff749711e39757
Dusadee <dusadee.s@globish.co.th>
2022-01-31 19:38:13 +0700

    add bull ui

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { BullModule } from '@nestjs/bull';
 import { Module, Logger } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { BullConfigService } from 'configs/bull.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloConsumer } from './processors/manager-a.processor';
 import { WriteConsumer } from './processors/manager-b.processor';
+import RedisConfig from 'configs/redis.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ load: [RedisConfig] }),
     BullModule.registerQueueAsync({
       useClass: BullConfigService,
       name: 'managerA',
